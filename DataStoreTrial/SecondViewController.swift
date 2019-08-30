b45f39708aea415499ad411914e475608a8505ba
tayutaedomo <tayutaedomo@gmail.com>
2019-08-30 22:32:42 +0900

    Add project base files

---
+++ DataStoreTrial/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DataStoreTrial
+//
+//  Created by tayutaedomo on 2019/08/30.
+//  Copyright © 2019 tayutaedomo.net. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
